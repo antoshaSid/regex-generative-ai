f5af3705f20f2c8bea536dda248aa6f182db7633
asidliar <asidliar@atlassian.com>
2023-09-11 00:30:58 +0300

    Implement simple UI

--- src/main/java/org/example/GenerativeAiProgram.java
+++ src/main/java/org/example/GenerativeAiProgram.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+import org.example.utils.StringUtils;
+
 public class GenerativeAiProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter password:");
+        final String password = scanner.next();
+
+        System.out.println("Enter maximum password length:");
+        final int length = scanner.nextInt();
+
+        System.out.println(StringUtils.isStrongPassword(password, length));
     }
 }
